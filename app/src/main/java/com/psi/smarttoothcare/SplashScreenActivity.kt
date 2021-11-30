6b27af98a394ab96e91d68907811ce415de0b77f
bagus2x <dutapanggulu@gmail.com>
2021-11-30 07:57:57 +0700

    add splash screen

---
+++ app/src/main/java/com/psi/smarttoothcare/SplashScreenActivity.kt
@@ -0,0 +1,25 @@
+package com.psi.smarttoothcare
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.psi.smarttoothcare.databinding.ActivitySplashScreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 3000)
+    }
+}
