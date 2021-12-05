9603004df0bd6a30e28fd2528472d3b541f3874e
Azzara Aji Syahputri <64596207+ayaazzara@users.noreply.github.com>
2021-12-05 08:39:44 +0700

    Create HistoryViewModelNew.kt
    Add Function Delete

---
+++ app/src/main/java/com/psi/smarttoothcare/ui/history/HistoryViewModelNew.kt
@@ -0,0 +1,50 @@
+package com.psi.smarttoothcare.ui.history
+
+import androidx.lifecycle.ViewModel
+import com.psi.smarttoothcare.model.History
+import com.psi.smarttoothcare.repository.HistoryRepository
+import com.psi.smarttoothcare.utils.plusAssign
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryViewModel @Inject constructor(private val historyRepository: HistoryRepository) : ViewModel() {
+    val histories = historyRepository.observeHistories()
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
+
+    fun create(history: History) {
+        compositeDisposable += historyRepository.create(history).observeOn(AndroidSchedulers.mainThread()).subscribe({
+
+        }) {
+            Timber.e(it)
+        }
+    }
+
+    fun update(history: History) {
+        val o = Observable.create<String> {
+
+        }
+
+        compositeDisposable += historyRepository.update(history)
+            .observeOn(AndroidSchedulers.mainThread()).subscribe({
+        }) {
+            Timber.e(it)
+        }
+    }
+
+    fun delete(history: History) {
+        compositeDisposable.add(historyRepository.delete(history).subscribe({
+        }) {
+            Timber.e(it)
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.dispose()
+    }
+}
